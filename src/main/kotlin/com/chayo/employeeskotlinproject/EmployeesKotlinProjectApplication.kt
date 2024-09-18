88f2144c4c5586208e82ee906580fdb8caadb1d8
rosario_gordillo <rosario.gordillo@dreambox.com>
2024-09-18 13:17:49 -0600

    EMPKOT-01 Upload project

---
+++ src/main/kotlin/com/chayo/employeeskotlinproject/EmployeesKotlinProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.chayo.employeeskotlinproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeesKotlinProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmployeesKotlinProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/chayo/employeeskotlinproject/EmployeesKotlinProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chayo.employeeskotlinproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeesKotlinProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
